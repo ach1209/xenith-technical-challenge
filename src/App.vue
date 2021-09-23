e0626249c0b654d05e65d65bd6f58b20c3072a60
Anthony <ach0074@gmail.com>
2021-09-22 20:46:56 -0400

    Added initial styling for todo app and created placeholder elements

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,76 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="todo-header">
+    <h1>Todo</h1>
+    <form>
+      <!-- Need button here -->
+      <input type="text" name="todo-input" id="todo-input" placeholder="Create a new item...">
+    </form>  
+  </div>
+  <div class="todo-container">
+    <ul v-if="todos.length">
+      <todo-item></todo-item>
+    </ul>
+    <p v-else>You have no items in your todo list. Create an item to begin tracking your list.</p>
+  </div>
 </template>
 
 <script>
+import TodoItem from './components/Todo/TodoItem.vue'
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      todos: []
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 #app {
+  max-width: 80rem;
+  min-height: 100vh;
+  margin: 0 auto;
+}
+
+.todo-header {
+  width: 100%;
+
+  h1 {
+    color: $white;
+    text-transform: uppercase;
+    letter-spacing: 0.5rem;
+    margin: 2rem 0;
+  }
+}
+
+form {
+
+  input[type="text"] {
+    width: 100%;
+    height: 6rem;
+    background-color: $dark-bg;
+    border: none;
+    border-radius: 0.5rem;
+
+    &::placeholder {
+      color: $gray;
+      font-size: 2rem;
+      letter-spacing: 0.2rem;
+      opacity: 1; /* Resolves transparency in Firefox */
+    }
+
+    &:focus {
+
+      &::placeholder {
+        color: $white;
+      }      
+    }
+  }
 
 }
 </style>
---
+++ src/components/Todo/TodoItem.vue
@@ -0,0 +1,3 @@
+<template>
+  <li>Todo item</li>
+</template>
